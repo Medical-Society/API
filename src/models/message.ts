6e9eb622fa782fc12c52172e4af78d77edf4a1c2
Mina Magdy <mina.magdy.takawey@gmail.com>
2024-06-25 19:33:45 +0300

    Add createdAt in messages, default: current time

--- src/models/message.ts
+++ src/models/message.ts
@@ -12,8 +12,11 @@ export class Message {
   @prop({ required: true })
   text: string;
 
-  @prop({ default: false ,required: true})
+  @prop({ default: false, required: true })
   seen: boolean;
+
+  @prop({ default: new Date() })
+  createdAt?: Date;
 }
 const MessageModel = getModelForClass(Message);
 export default MessageModel;
